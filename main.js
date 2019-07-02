9f57770834566f940eab2758530bdda63efe8370
Chris Basham <chrisdbasham0317@gmail.com>
2019-07-02 16:26:34 -0600

    Add actual copy of pseudo code in js file

---
+++ main.js
@@ -0,0 +1,14 @@
+//Global Variables
+	//var p1Name = document.querySelector(".")
+
+//Add Event Listeners
+	//Set Range: on click, grab inputs, start respective function
+	//Submit Guess: on click, grab guess inputs, start respective function
+
+
+//Functions
+
+	//Set Range
+		//Text 
+
+	//Submit Guess
