1054e8e3ae38983c41bede2f616a25232455cfae
Eric Meldrum <eric.meldrum@gmail.com>
2019-07-02 17:11:21 -0600

    Add pseudocode to map out functions, variables, event listeners for Phase 1

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 //Global Variables
-	//var p1Name = document.querySelector(".")
+  //var p1Name = document.querySelector(".")
 
 //Add Event Listeners
-	//Set Range: on click, grab inputs, start respective function
-	//Submit Guess: on click, grab guess inputs, start respective function
+  //Set Range: on click, grab inputs, start respective function
+  //Submit Guess: on click, grab guess inputs, start respective function
+  //Reset Game: on click, clears inputs, start respective function
+  //Clear game: on click, clears inputs
+  //Timer
+  //Keep track of on clicks; multiply total clicks x2
 
 
 //Functions
+  //Set Range
+    //If (lowRange < highRange) {Populate minimum range to #lowRange, populate max range to #highRange} else {return error message}
 
-	//Set Range
-		//Text 
+  //Submit Guess
+    //Random Number Generator
+    //grab min/max ranges from Set Range function, generate random number within that range.
+    //Compare guess 1 and guess 2 to a random number
+    //Populate guesses to #player1-guess and #player2-guess, respectively
+    //if (#guess1 < randomNumber) {return "that's too low"} else if (guess1 > randomNumber) {return "that's too high"} else {return "BOOM!"}
+    //repeat for player 2
+    //Generates new card
+    
 
-	//Submit Guess
+  //Disable buttons
+    //Button default is disabled; adds style class when inputs are !== ""
