0a105c794d99429b2651527c8a51a03976b7187e
Chris Basham <chrisdbasham0317@gmail.com>
2019-07-10 15:33:08 -0600

    Add classes to html, styles for disabled btns, funcs for disable btns

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ var randomNum = null;
 
 setRangeBtn.addEventListener('click', updateRanges)
 
+
 function updateRanges(event) {
   event.preventDefault();
   if (parseInt(rangeMin.value) < parseInt(rangeMax.value)) {
@@ -69,7 +70,13 @@ var person2Name = document.querySelector('#player2-name');
 
 var submitGuessBtn = document.querySelector('#submit-guess-button')
 
+var fields = document.querySelector('#guess1');
+             document.querySelector('#guess2');
+             document.querySelector('#player1-name');
+             document.querySelector('#player2-name');
+
 submitGuessBtn.addEventListener('click', submitGuess)
+fields.addEventListener('keyup', disableButton)
 
 function submitGuess(event) {
   event.preventDefault();
@@ -121,6 +128,44 @@ function createCards(winner, loser) {
   <h3 id='winner-name'> ${winner} </h3>
   <p class='winner'>WINNER</p></div>`;
   document.querySelector('#card-holder').insertAdjacentHTML('afterbegin', winnerCard);
+}
+
+// function disableButton() {
+//    if(document.querySelectorAll('input[type=text]').value === "" || document.querySelectorall('input[type=number]').value) === "" { 
+//             document.querySelector('#reset-game-button').disabled = true;
+//             document.queryselector('#clear-game-button').disabled = true; 
+//         } else { 
+//             document.querySelector('#reset-game-button').disabled = false;
+//             document.queryselector('#clear-game-button').disabled = false;
+//         }
+//     }
+
+function disableButton() {
+  if (person1Name !== '' || person1Guess !== '' || person2Name !== '' || person2Guess !== '') {
+    for (var i = 0; i < resetButtons.length; i++) {
+    resetButtons[i].disabled = false;
+    }
+    goodBtn();
+  } else {
+    for (var i = 0; i < resetButtons.length; i++) {
+    resetButtons[i].disabled = true;
+    }
+    grayBtn();
+  }
+}
+
+var resetButtons = document.querySelectorAll('.clear-btns');
+
+function grayBtn() {
+  for (var i = 0; i < resetButtons.length; i++) {
+    resetButtons[i].classList.add('gray-out');
+  }
+}
+
+function goodBtn() {
+    for (var i = 0; i < resetButtons.length; i++) {
+    resetButtons[i].classList.remove('gray-out');
+  }
 }
 
 
