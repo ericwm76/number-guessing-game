6645b31e4ad80761af292d9a49714a9262b56770
Eric Meldrum <eric.meldrum@gmail.com>
2019-07-10 18:00:14 -0600

    Refactor functions for error messages in submit guesses field

--- main.js
+++ main.js
@@ -12,20 +12,12 @@ var person2Name = document.querySelector('#player2-name');
 var submitGuessBtn = document.querySelector('#submit-guess-button');
 var rangeMinMsg = document.querySelector('#range-error-message1');
 var rangeMaxMsg = document.querySelector('#range-error-message2');
-var nameMsg = 'Enter a name';
-var guessMsg1 = 'Enter a number';
-var guessMsg2 = 'Guess is not in range';
-
-// var winner = null;
-
-// var loser = null;
-
-// console.log(winner)
-
-// Setting the Range
-// rangeMin.addEventListener('keyup', checkMinRange)
-// rangeMax.addEventListener('keyup', checkMaxRange)
-setRangeBtn.addEventListener('click', checkRangeInputs)
+var name1Msg = document.querySelector('#name-error-message1')
+var name2Msg = document.querySelector('#name-error-message2')
+var guess1Msg = document.querySelector('#guess-error-message1')
+var guess2Msg = document.querySelector('#guess-error-message2')
+var blankMsg = 'Field cannot be blank';
+var guessMsg = 'Enter a number within the range';
 
 function errorMsgAppear(inputField, errorField, errorMsg) {
   inputField.classList.add('text-input-error');
@@ -38,48 +30,56 @@ function errorMsgDisappear(inputField, errorField) {
   errorField.classList.add('error-message-hidden');
 }
 
-function checkMinRange(e) {
-  event.preventDefault();
-  if (rangeMin.value === '') {
-    errorMsgAppear(rangeMin, rangeMinMsg, guessMsg1);
-  } 
-}
+// Setting the Range
+setRangeBtn.addEventListener('click', checkRangeInputs)
 
-function checkMaxRange(e) {
-  event.preventDefault();
-  if (rangeMax.value === '') {
-    errorMsgAppear(rangeMax, rangeMaxMsg, guessMsg1);
-  } 
-} 
 
 function checkRangeInputs(e) {
   event.preventDefault();
+  // errorMsgDisappear(rangeMin, rangeMinMsg);
+  // errorMsgDisappear(rangeMax, rangeMaxMsg);
+  // rangeValidator = 0
   checkMinRange();
   checkMaxRange();
   // if (checkMinRange === true && checkMaxRange === true) {
-  console.log('checkRangeInputs ran');
-  validateRange();
-}
+    console.log('checkRangeInputs ran');
+    validateRange();
+  }
 // }
+function checkMinRange() {
+  // event.preventDefault();
+  if (rangeMin.value === '') {
+    errorMsgAppear(rangeMin, rangeMinMsg, blankMsg);
+  } 
+  // else {rangeValidator++}
+}
 
-function validateRange(e) {
-  event.preventDefault();
+function checkMaxRange() {
+  // event.preventDefault();
+  if (rangeMax.value === '') {
+    errorMsgAppear(rangeMax, rangeMaxMsg, blankMsg);
+  } 
+  // else {rangeValidator++}
+} 
+
+function validateRange() {
+  // event.preventDefault();
   if (rangeMin.value >= rangeMax.value) {
       errorMsgAppear(rangeMin, rangeMinMsg, 'Min must be less than max');
   } else {
+    // rangeValidator++;
     console.log('validateRange ran')
     updateRange();
   }
 }  
 
-function updateRange(e) {
-  event.preventDefault();
+function updateRange() {
+  // event.preventDefault();
+  // if (rangeValidator >= 3) {}
     lowerRange.innerHTML = rangeMin.value;
     upperRange.innerHTML = rangeMax.value; 
     randomNum = Math.floor(Math.random() * (parseInt(rangeMax.value) - parseInt(rangeMin.value) + 1)) + parseInt(rangeMin.value);
     console.log('updateRange ran')
-    errorMsgDisappear(rangeMin, rangeMinMsg);
-    errorMsgDisappear(rangeMax, rangeMaxMsg);
   } 
 
 // Submitting Names and Guesses
@@ -87,59 +87,108 @@ submitGuessBtn.addEventListener('click', checkGuessInputs)
 
 function checkName1(e) {
   event.preventDefault();
-  if (rangeMin.value === '') {
-    errorMsgAppear(rangeMin, rangeMinMsg, guessMsg1);
+  if (person1Name.value === '') {
+    errorMsgAppear(person1Name, name1Msg, blankMsg);
   } 
+  // else {submitFieldsCounter++}
+}
+
+function checkName2(e) {
+  event.preventDefault();
+  if (person2Name.value === '') {
+    errorMsgAppear(person2Name, name2Msg, blankMsg);
+  } 
+  // else {submitFieldsCounter++}
+}
+
+function checkGuess1(e) {
+  event.preventDefault();
+  if (person1Guess.value === '') {
+    errorMsgAppear(person1Guess, guess1Msg, blankMsg);
+  } 
+  // else {submitFieldsCounter++}
+}
+
+function checkGuess2(e) {
+  event.preventDefault();
+  if (person2Guess.value === '') {
+    errorMsgAppear(person2Guess, guess2Msg, blankMsg);
+  } 
+  // else {submitFieldsCounter++}
 }
 
 function checkGuessInputs(e) {
   event.preventDefault();
-  console.log('checkGuessInputs ran')
-  if (person1Name.value === "") {
-    document.querySelector('#name-error-message1').innerText = nameMsg;
-  } 
-  if (person2Name.value === "") {
-    document.querySelector('#guess-error-message1').innerText = guessMsg1;
-  } 
-  if (person1Guess.value === "") {
-    document.querySelector('#name-error-message2').innerText = nameMsg;
-  }
-  if (person2Guess.value === ""){
-    document.querySelector('#guess-error-message2').innerText = guessMsg1;
-  } 
-  else {
-    validateGuesses(event);
-  }
+  console.log('checkGuessInputs ran');
+  checkName1();
+  checkName2();
+  checkGuess1();
+  checkGuess2();
+  // if (submitFieldsCounter >= 4) {
+  validateGuesses();
+  // }
 }
+//   if (person1Name.value === "") {
+//     document.querySelector('#name-error-message1').innerText = blankMsg;
+//   } 
+//   if (person2Name.value === "") {
+//     document.querySelector('#guess-error-message1').innerText = blankMsg;
+//   } 
+//   if (person1Guess.value === "") {
+//     document.querySelector('#name-error-message2').innerText = blankMsg;
+//   }
+//   if (person2Guess.value === ""){
+//     document.querySelector('#guess-error-message2').innerText = blankMsg;
+//   } 
+//   else {
+//     validateGuesses(event);
+//   }
+// }
 
 // var fields = document.querySelector('#guess1');
 //              document.querySelector('#guess2');
 //              document.querySelector('#player1-name');
 //              document.querySelector('#player2-name');
 
-// submitGuessBtn.addEventListener('click', submitGuess)
 // fields.addEventListener('keyup', disableButton)
 
-function validateGuesses(event) {
+
+function verifyGuess1(e) {
+  event.preventDefault();
+  if (person1Guess.value === NaN && parseInt(rangeMin.value) <= person1Guess.value <= parseInt(rangeMax.value)) {
+    errorMsgAppear(person1Guess, guess1Msg, guessMsg);
+  } 
+  // else {submitFieldsCounter++}
+}
+
+function verifyGuess2(e) {
+  event.preventDefault();
+  if (person2Guess.value === NaN && parseInt(rangeMin.value) <= person2Guess.value <= parseInt(rangeMax.value)) {
+    errorMsgAppear(person2Guess, guess2Msg, guessMsg);
+  } 
+  // else {submitFieldsCounter++}
+}
+
+function validateGuesses(e) {
 event.preventDefault();
-console.log('validateGuess ran')
+console.log('validateGuesses ran')
   if (person1Guess.value === NaN) {
-    document.querySelector('#guess-error-message1').innerText = guessMsg1;
+    document.querySelector('#guess-error-message1').innerText = guessMsg;
   }
   if (person2Guess.value === NaN) {
-    document.querySelector('#guess-error-message2').innerText = guessMsg1;
+    document.querySelector('#guess-error-message2').innerText = guessMsg;
   }
   if (person1Guess.value < rangeMin.value) {
-    document.querySelector('#guess-error-message1').innerText = guessMsg2;
+    document.querySelector('#guess-error-message1').innerText = guessMsg;
   }
   if (person1Guess.value > rangeMax.value) {
-    document.querySelector('#guess-error-message1').innerText = guessMsg2;
+    document.querySelector('#guess-error-message1').innerText = guessMsg;
   }
   if (person2Guess.value < rangeMin.value) {
-    document.querySelector('#guess-error-message2').innerText = guessMsg2;
+    document.querySelector('#guess-error-message2').innerText = guessMsg;
   }
   if (person2Guess.value > rangeMax.value) {
-    document.querySelector('#guess-error-message2').innerText = guessMsg2;
+    document.querySelector('#guess-error-message2').innerText = guessMsg;
   }
   else {
     submitGuess(event);
@@ -196,16 +245,6 @@ function createCards(winner, loser) {
   <p class='winner'>WINNER</p></div>`;
   document.querySelector('#card-holder').insertAdjacentHTML('afterbegin', winnerCard);
 }
-
-// function disableButton() {
-//    if(document.querySelectorAll('input[type=text]').value === "" || document.querySelectorall('input[type=number]').value) === "" { 
-//             document.querySelector('#reset-game-button').disabled = true;
-//             document.queryselector('#clear-game-button').disabled = true; 
-//         } else { 
-//             document.querySelector('#reset-game-button').disabled = false;
-//             document.queryselector('#clear-game-button').disabled = false;
-//         }
-//     }
 
 function disableButton() {
   if (person1Name !== '' || person1Guess !== '' || person2Name !== '' || person2Guess !== '') {
